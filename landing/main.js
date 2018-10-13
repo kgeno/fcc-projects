f220ba816b544876b7b6c2d12dd91f64b11cfed8
Geno <geno@ezfuse.com>
2018-10-13 14:23:24 -0700

    update: more responsivness

--- landing/main.js
+++ landing/main.js
@@ -15,3 +15,12 @@
 	});
 });
 }(jQuery));
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
