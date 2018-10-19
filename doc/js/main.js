758966b41772c4fd659c1f7c0172a5fa575fc2d2
Geno Kozniuk <geno@ezfuse.com>
2018-10-18 18:50:52 -0700

    update: added responsiveness and mobile layout

--- doc/js/main.js
+++ doc/js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
     $('.expand').click(function() {
         $(this).siblings('.sub-chapter:first').toggle('fast')
-  });
+    });
+    $('.hamburger').click(function() {
+        $(this).siblings('.chapters:first').toggle('fast')
+    });
 });
